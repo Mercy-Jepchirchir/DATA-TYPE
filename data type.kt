d95bd631484d75b19b34c85d8d9258cdd2ed5742
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-02-12 15:57:51 +0300

    first commit

---
+++ data type.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var student  = ("mercy kemboi")
+    println(student)
+
+    var age :Int =(22)
+    println(age)
+
+    var number:Int= 713520935
+    println(number)
+
+    var weightkg:Float= 57.5F
+    println(weightkg)
+
+    var isakenyan:Boolean= true
+    println(isakenyan)
+
+
+}
